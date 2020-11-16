cd855237e66ea4a34df05001698c4556c05ddd57
Bernardo <bmrocco3000@gmail.com>
2020-11-16 18:11:23 -0300

    fc

---
+++ server.py
@@ -0,0 +1,61 @@
+LISTEN_ADDRESS = ('0.0.0.0', 8080)
+
+import websockets
+import asyncio
+import json
+
+# The set of clients connected to this server. It is used to distribute
+# messages.
+clients = {} #: {websocket: name}
+
+@asyncio.coroutine
+def client_handler(websocket, path):
+    print('Novo cliente', websocket)
+    print(' ({} cilentes online)'.format(len(clients)))
+    mt = {'mt':'clients', 'data':', '.join(list(clients.values()))}
+    yield from websocket.send(json.dumps(mt))
+
+    # The first line from the client is the name
+    name = yield from websocket.recv()
+    mt = {'mt':'serv', 'data':'Bem vindo ao chat, {}!'.format(name)}
+    yield from websocket.send(json.dumps(mt))
+    clients[websocket] = name
+    for client, _ in clients.items():
+        mt = {'mt':'clients', 'data':', '.join(list(clients.values()))}
+        yield from client.send(json.dumps(mt))
+        if not client == websocket:
+            mt = {'mt':'serv', 'data':name + ' se juntou ao chat'}
+            yield from client.send(json.dumps(mt))
+
+    # Handle messages from this client
+    while True:
+        try:
+            message = yield from websocket.recv()
+        except:
+            their_name = clients[websocket]
+            del clients[websocket]
+            print('Client closed connection', websocket)
+            for client, _ in clients.items():
+                mt = {'mt':'clients', 'data':', '.join(list(clients.values()))}
+                yield from client.send(json.dumps(mt))
+                mt = {'mt':'serv', 'data':name + ' saiu do chat'}
+                yield from client.send(json.dumps(mt))
+            break
+        # Send message to all clients
+        if message[0] != "\\":
+            for client, _ in clients.items():
+                mt = {'mt':'mess', 'data':'{}: {}'.format(name, message)}
+                yield from client.send(json.dumps(mt))
+        else:
+            to = message[1:].split(' ')[0]
+            a = next((server for server, nome in clients.items() if nome == to), None)
+            mt = {'mt':'pm', 'data':'[Private] {}: {}'.format(name, ' '.join(message.split(' ')[1:]))}
+            yield from a.send(json.dumps(mt))
+            mt = {'mt':'pm', 'data':'[Private to {}] {}: {}'.format(to, name, ' '.join(message.split(' ')[1:]))}
+            yield from websocket.send(json.dumps(mt))
+           
+
+start_server = websockets.serve(client_handler, *LISTEN_ADDRESS)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ ws.js
@@ -0,0 +1,82 @@
+var server = 'ws://localhost:8080/';
+
+function zPad(n) {
+    if (n < 10) return '0' + n;
+    else return n;
+}
+
+function timestamp() {
+    var d = new Date();
+    return zPad(d.getHours()) + ':' + zPad(d.getMinutes()) + ':' + zPad(d.getSeconds());
+}
+
+function write_to_mbox(message) {
+    var mt = message.mt;
+    var line = '[' + timestamp() + '] ' + message.data + '<br>';
+    if (mt === "serv"){
+        var line = line.fontcolor('green')
+    }
+    else if (mt === "pm"){
+        var line = line.fontcolor('blue')
+    }
+    $('#messages').append(line);
+}
+
+$(document).ready(function() {
+    var socket = new WebSocket(server);
+    var clients = [];
+    $('#name').focus();
+    
+    socket.onerror = function(error) {
+        console.log('WebSocket Error: ' + error);
+    };
+
+    socket.onclose = function(event) {
+        write_to_mbox({'mt':'serv', 'data':'Desconectado do Web Chat'});
+    };
+
+    socket.onmessage = function(event) {
+        var recv = JSON.parse(event.data);
+        console.log(recv);
+        if (recv.mt === 'clients'){
+            if (!recv.data){
+                $('#connected').text('Sem usuários conectados');
+            }
+            else{
+                clients = recv.data.split(', ')
+                $('#connected').text(''.concat('Usuarios conectados: ', recv.data));
+            };    
+        }
+        else{
+            write_to_mbox(recv)
+        };
+    };
+
+    $('#message-form').submit(function() {
+        socket.send($('#message').val());
+        $('#message').val('');
+        return false;
+    });
+
+    $('#connect-form').submit(function() {
+        var name = $('#name').val().trim();
+        if (clients.includes(name)){
+            alert("Nome já utilizado, escolha outro")
+        }
+        else if (name.includes(' ')){
+            alert("Não são permitidos espaços no nome de usuário")
+        }
+        else{
+            socket.send(name);
+            
+            $('#jumbotron').hide();
+            $('#ttl').text("Web Chat");
+
+            write_to_mbox({'mt':'serv', 'data':"Conectado ao chat!"});
+            
+            $('#message_wrapper').show();
+            $('#message').focus();
+        };
+        return false;
+    });
+});
